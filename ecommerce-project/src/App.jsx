de3e1006e651225eac823a5b8e422387d861a7b3
williamimmanuelv <williamimmanuelv.com>
2025-09-06 13:37:39 +0530

    Generate the HTML for the orders page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -25,9 +25,9 @@ function App() {
   return (
     <>  
     <Routes>
-      <Route index element={<HomePage cart={cart}/>}/>
-      <Route path='checkout' element={<CheckoutPage cart={cart}/>}/>
-      <Route path='orders' element={<OrdersPage/>}/>
+      <Route index element={<HomePage cart={ cart }/>}/>
+      <Route path='checkout' element={<CheckoutPage cart={ cart }/>}/>
+      <Route path='orders' element={<OrdersPage cart={ cart }/>}/>
       <Route path='tracking' element={<TrackingPage/>}/>
       <Route path='*' element={<NotFound/>}/>
     </Routes>
