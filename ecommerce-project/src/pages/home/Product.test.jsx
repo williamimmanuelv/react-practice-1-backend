8486a240c2236c42427a0ada3c21748245e71bf3
williamimmanuelv <williamimmanuelv.com>
2025-09-19 14:47:41 +0530

    Use beforeEach to remove Duplicate

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,4 +1,4 @@
-import { it, expect, describe, vi } from 'vitest';
+import { it, expect, describe, vi, beforeEach } from 'vitest';
 // screen lets us check the fake web page
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
@@ -8,8 +8,13 @@ import axios from 'axios';
 vi.mock('axios');
 
 describe('Product component', () => {
-    it('displays the product details correctly', () => {
-        const product = {
+
+    let product;
+    // vi.fn = creates a fake function that doesn't do anything.
+    let loadCart;
+
+    beforeEach(() => {
+        product = {
             id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
             image: "images/products/athletic-cotton-socks-6-pairs.jpg",
             name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -19,9 +24,13 @@ describe('Product component', () => {
             },
             priceCents: 1090,
             keywords: ["socks", "sports", "apparel"]
-        }
-        // vi.fn = creates a fake function that doesn't do anything.
-        const loadCart = vi.fn();
+        };
+        loadCart = vi.fn();
+    })
+
+
+
+    it('displays the product details correctly', () => {
 
         render(<Products product={product} loadCart={loadCart} />)
 
@@ -45,20 +54,8 @@ describe('Product component', () => {
         ).toBeInTheDocument();
     })
 
-    it('adds a product to the cart', async() => {
-        const product = {
-            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-            rating: {
-                stars: 4.5,
-                count: 87
-            },
-            priceCents: 1090,
-            keywords: ["socks", "sports", "apparel"]
-        }
-        // vi.fn = creates a fake function that doesn't do anything.
-        const loadCart = vi.fn();
+    it('adds a product to the cart', async () => {
+
 
         render(<Products product={product} loadCart={loadCart} />)
 
